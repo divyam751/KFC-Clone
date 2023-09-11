b533503b989b7a35d75fc741c24b6d8bb66fdc29
Divyam Chauhan <125983433+divyam751@users.noreply.github.com>
2023-09-11 10:05:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
         />
       )}
       <Hero />
+      <Home />
     </div>
   );
 }
--- src/pages/home/home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Style.css";
-
-const home = () => {
-  return <div>home</div>;
-};
-
-export default home;
