2a4b3a281fd62dc4aa880c728407c196b854f4aa
Divyam Chauhan <125983433+divyam751@users.noreply.github.com>
2023-09-11 22:28:34 +0530

    Carousel fixed

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,8 +3,6 @@ import "./Style.css";
 
 const Carousel = () => {
   const arr = [
-    "https://images.ctfassets.net/wtodlh47qxpt/5gf6cz2vMpCLgQ4M4XaUaj/8a10d03ec85203e7439e113d336f949d/Zinger_pro_1440x396px.jpg?w=1440&fit=fill&fm=webp",
-    "https://images.ctfassets.net/wtodlh47qxpt/7K2vLQBK4gYPuHKqQ24F4Z/ee7c4a43c09d3e7500cd80aebed74475/Double_Down_1440x396px.jpg?w=1289&fit=fill&fm=webp",
     "https://scontent.fagr1-4.fna.fbcdn.net/v/t39.30808-6/344377015_204365952379795_1104948206954739779_n.png?_nc_cat=103&ccb=1-7&_nc_sid=52f669&_nc_ohc=jozQ-qMaJtAAX_4HjMn&_nc_ht=scontent.fagr1-4.fna&oh=00_AfBOJs_i2aWyZw4BnVUafZlFQODl4LCmGRmyIdqbWxhYSA&oe=6502A2BE",
     "https://scontent.fagr1-1.fna.fbcdn.net/v/t39.30808-6/340955298_779571883726528_8243326874128589709_n.png?_nc_cat=104&ccb=1-7&_nc_sid=52f669&_nc_ohc=yhP2c4aWp-QAX96pMpu&_nc_ht=scontent.fagr1-1.fna&oh=00_AfDBATGZ5xwDmDtvn6_UwQsVvJ-ATm3N3n7OZGVr3z50fA&oe=65035E4C",
     "https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/335483946_2352483978256580_2567174708211694457_n.png?_nc_cat=111&ccb=1-7&_nc_sid=52f669&_nc_ohc=ZTK5e3ptIWoAX98jqsG&_nc_oc=AQm_tn48sBUTlR8dAIT7li8QMoj1hY6Y-eUVP7HYKKwkq5Z9Pq8MGCuXtgIWt6nY2bGMaI2ObsXpWO_RJQuck6DG&_nc_ht=scontent.fagr1-3.fna&oh=00_AfCpeZwRRbZcgI6NFY43N85EZbnKwGqx3X0PfgGfz6bjCg&oe=6503667D",
